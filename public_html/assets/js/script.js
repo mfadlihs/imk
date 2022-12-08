3359fd1382f0e52ebcf8480aee8a680729c137d4
mfadlihs <winnerfadli999@gmail.com>
2022-12-08 17:11:19 +0700

    functional searchbar

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -27,14 +27,23 @@ function changeActivePosition(activeItem) {
 	activeItem.classList.add("active");
 }
 
+function getTitle(node) {
+	return node.children[1].children[1].childNodes[0].data;
+}
+
 const Searchbar = document.querySelector(".form-control");
+console.log(allCategoryPosts[0].children[1].children[1].childNodes[0].data);
 
 const filterSearch = () => {
 	console.log(Searchbar.value);
 	var item = Searchbar.value == "" ? "all" : Searchbar.value;
 	// filterPosts(Searchbar.value);
+
 	for (let i = 0; i < allCategoryPosts.length; i++) {
-		if (allCategoryPosts[i].classList.contains(item)) {
+		if (
+			allCategoryPosts[i].classList.contains(item) ||
+			getTitle(allCategoryPosts[i]).toLowerCase().includes(Searchbar.value)
+		) {
 			allCategoryPosts[i].style.display = "block";
 		} else {
 			allCategoryPosts[i].style.display = "none";
